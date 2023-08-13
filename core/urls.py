341947085111dc08fdcd67fee9759ee3a0377d76
Your Name <you@example.com>
2023-08-13 14:41:47 +0330

    insert basket

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,8 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls', namespace='store')),
+    path('basket/',include('basket.urls', namespace='basket')),
+    
     
 ]
 
