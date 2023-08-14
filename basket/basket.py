0a2cff1063cf37a10ac39c57583ee182bd2d432e
Your Name <you@example.com>
2023-08-14 16:16:55 +0330

    Basket

---
+++ basket/basket.py
@@ -0,0 +1,14 @@
+
+
+
+class Basket():
+    
+    def __init__(self, request):
+        
+        self.session = request.session
+        basket = self.session.get('skey')
+        if 'skey' not in request.session:
+            basket = self.session['skey'] = {'number' : 1231231 }
+        
+        self.basket = basket
+        pass
---
+++ basket/context_processors.py
@@ -0,0 +1,9 @@
+from .basket import Basket
+
+
+
+def basket(request):
+    return {
+        'basket': Basket(request)
+    }
+    
